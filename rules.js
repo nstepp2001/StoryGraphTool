edf122c56423e257172de92d7bb97efec6048a2d
nstepp2001 <nstepp@ucsc.edu>
2022-04-25 16:58:31 -0700

    initial location is good

--- rules.js
+++ rules.js
@@ -1,19 +1,18 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData["Title"]); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData["InitialLocation"]); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
+        let locationData = this.engine.storyData["Locations"]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(this.engine.locationData[key]); // TODO: replace this text by the Body of the location data
         if(true) { // TODO: check if the location has any Choices
             for(let choice of ["example data"]) { // TODO: loop over the location's Choices
                 this.engine.addChoice("action text"); // TODO: use the Text of the choice
