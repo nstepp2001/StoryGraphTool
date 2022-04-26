6adc276fc783f29b8980aa7f95aa008a97ec3231
nstepp2001 <nstepp@ucsc.edu>
2022-04-25 17:19:43 -0700

    rules are now fixed

--- rules.js
+++ rules.js
@@ -1,21 +1,21 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle(this.engine.storyData["Title"]); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, this.engine.storyData["InitialLocation"]); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = this.engine.storyData["Locations"]; // TODO: use `key` to get the data object for the current story location
-        this.engine.show(this.engine.locationData[key]); // TODO: replace this text by the Body of the location data
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
+        let locationData = this.engine.storyData.Locations[key]; // TODO: use `key` to get the data object for the current story location
+        this.engine.show(locationData.Body); // TODO: replace this text by the Body of the location data
+        if(locationData.Choices != null) { // TODO: check if the location has any Choices
+            for(let choice of locationData.Choices) { // TODO: loop over the location's Choices
+                this.engine.addChoice(choice.Text, choice); // TODO: use the Text of the choice
                 // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
             }
         } else {
